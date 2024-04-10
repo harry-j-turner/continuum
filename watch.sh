9ca951f6174ddab63a4f570aa07fc9fd12830eac
harry-optimised <henry.j.turner@gmail.com>
2024-04-10 21:33:27 +0100

    Update docker-compose path in the watch script.

--- watch.sh
+++ watch.sh
@@ -10,8 +10,8 @@ output=$(docker pull 664735937512.dkr.ecr.eu-west-2.amazonaws.com/continuum:late
 if [[ $output == *"Downloaded newer image for"* ]] || [[ $output == *"Status: Downloaded newer image"* ]]; then
   echo "Downloading newer image."
   # Use docker-compose with the production yml file to restart the services
-  docker-compose -f docker-compose.prod.yml down
-  docker-compose -f docker-compose.prod.yml up -d
+  /usr/local/bin/docker-compose -f docker-compose.prod.yml down
+  /usr/local/bin/docker-compose -f docker-compose.prod.yml up -d
 else
   echo "Image is up to date. No restart needed."
 fi
