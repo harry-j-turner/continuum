46c6a1520687f675f11cfae3d84119cd6942d18e
harry-optimised <henry.j.turner@gmail.com>
2024-04-11 17:30:29 +0100

    Bump version to 0.1.1

--- continuum/continuum/version.py
+++ continuum/continuum/version.py
@@ -1,3 +1,19 @@
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
-VERSION_NOTES = [{"version": "0.1.0", "date": "2024-04-10", "fixes": ["None"], "features": ["Initial release."]}]
+VERSION_NOTES = [
+    {
+        "version": "0.1.1",
+        "date": "2024-04-11",
+        "fixes": [
+            "Email report was not properly collecting last seven days of thoughts.",
+            "Thought entry textbox was not updating state properly.",
+        ],
+        "features": ["None."],
+    },
+    {
+        "version": "0.1.0",
+        "date": "2024-04-10",
+        "fixes": ["None"],
+        "features": ["Initial release."],
+    },
+]
--- ui/src/version.ts
+++ ui/src/version.ts
@@ -5,8 +5,17 @@ type Version = {
   features: string[];
 };
 
-export const version = '0.1.0';
+export const version = '0.1.1';
 export const versionNotes: Version[] = [
+  {
+    version: '0.1.1',
+    releaseDate: new Date('2024-04-11'),
+    fixes: [
+      'Email report was not properly collecting last seven days of thoughts.',
+      'Thought entry textbox was not updating state properly.'
+    ],
+    features: ['None']
+  },
   {
     version: '0.1.0',
     releaseDate: new Date('2024-04-10'),
