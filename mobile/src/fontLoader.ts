7ee12e591b62e407bb0a3cebf1c7e7a0f98debac
Harry Turner <harry.turner@ultraleap.com>
2024-06-08 15:40:03 +0100

    Replace copied app with expo default project for a clean start.

--- mobile/src/fontLoader.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as Font from 'expo-font';
-
-export async function loadFonts(): Promise<void> {
-  await Font.loadAsync({
-    'Roboto-Light': require('../assets/fonts/Roboto-Light.ttf')
-  });
-}
